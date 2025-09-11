08fbf127f9bbc047278d20926429dbf4d2d69c73
JoshuaSowerby <josh.d.sowerby@gmail.com>
2025-09-11 17:20:16 +1000

    more error fixes

--- oqsprov/oqs_hyb_kem.c
+++ oqsprov/oqs_hyb_kem.c
@@ -240,6 +240,9 @@ static int oqs_hyb_kem_encaps(void *vpkemctx, unsigned char *ct, size_t *ctlen,
     clock_gettime(CLOCK_MONOTONIC, &end);
     _elapsed_ns = (end.tv_sec - start.tv_sec) * 1000000000L +
                  (end.tv_nsec - start.tv_nsec);
+    const OQSX_EVP_CTX *evp_ctx = pkemctx->kem->oqsx_provider_ctx.oqsx_evp_ctx;
+    const OQS_KEM *qs_ctx = pkemctx->kem->oqsx_provider_ctx.oqsx_qs_ctx.kem;
+
     const char *_evp_name = OBJ_nid2sn(evp_ctx->evp_info->keytype);
     const char *pq_name = qs_ctx->method_name;
     printf("%s_%s encaps time: %ldns\n",
